9fafa1b2d705745cfe71a8d920f28762d0a829c8
Muchen <bonpon830224@gmail.com>
2020-05-29 20:43:22 +0800

    question

--- src/main.js
+++ src/main.js
@@ -1,10 +1,40 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap'
+
+
 import router from './router'
+import App from './App.vue'
+
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+
+axios.defaults.withCredentals = true;
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+router.beforeEach((to, from, next) => {
+  console.log('to',to,'from',from,'next',next);
+  if(to.meta.requiresAuth){
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`;
+    axios.post(api).then((response) =>{
+      console.log(response.data);
+      if(response.data.success){
+        next();
+      }else{
+        next({
+          path:'/login',
+        })
+      }
+    })
+  } else{
+    next();
+  }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import Login from '@/components/pages/Login.vue'
+import Products from '@/components/pages/products'
+
+
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    //避免用戶進入不存在的頁面
+    path:'*',
+    redirect:'/login'
   },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  //   meta: { requiresAuth: true },
+  // },
   {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:Login
+  },
+  {
+    path: '/admin',
+    name: 'Home',
+    component: Dashboard,
+    children:[
+      {
+        path:'products',
+        name:'products',
+        component:Products,
+        meta: { requiresAuth: true },
+      }
+
+    ],
   }
 ]
 
